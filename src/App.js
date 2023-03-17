a954018cbe0e1470f5ce8f16eb296ca995bfb7ed
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 05:10:09 +0300

    Created the data array of strings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 const App = () =>
 {
+  let data = ["Fiat", "Peugeot", "Ford", "Renault", "Citroen"]
+
   return (
     <div className="App">
       <h1 className="bg-slate-500 text-white text-center">This is a tailwind styled site</h1>

f3a5a7e7d6717ed9732c3498b4b0b2aac36f1e09
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 05:23:38 +0300

    rendered out the data array

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ const App = () =>
   let data = ["Fiat", "Peugeot", "Ford", "Renault", "Citroen"]
 
   return (
-    <div className="App">
+    <div className="App max-w-3xl mx-auto h-full">
       <h1 className="bg-slate-500 text-white text-center">This is a tailwind styled site</h1>
+      <div>
+        {data.map((el) => 
+        {
+          return <div>Cars listed as <span className="font-bold">{el.toUpperCase()}</span></div>
+        })}
+      </div>
     </div>
   )
 }
